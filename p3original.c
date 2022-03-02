f8a08d24c070d390c90f358c9efcbfa6edd0fef8
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-03-02 04:47:36 +0000

    combination of ncr

---
+++ p3original.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int input_n_and_r(int *n, int *r)
+{
+  printf("enter the value of n and r\n");
+  scanf("%d %d", n , r);
+}
+int ncr(int n, int r)
+{ int i, result ,b,final = 1; 
+  int prod =1;
+  int run = 1;
+  int a = (n-r);
+  for(i=n;i>=1;i--)
+    {  
+       prod = prod*i;
+    }
+  for(i=r;i>=r;i--)
+    {
+      run = run*i;
+    }
+  for(i=a;i>=1;i--)
+    {
+      final = final*i;
+    }
+  b = final*run;
+  result = prod/b;
+  return result;
+  
+}
+void output(int n, int r, int result)
+{
+  printf("%d^C%d = %d\n",n,r,result);
+}
+int main()
+{
+  int n, r, result;
+   input_n_and_r(&n, &r);
+  result=  ncr(n,  r);
+   output( n, r,  result);
+  return 0;
+  
+}
